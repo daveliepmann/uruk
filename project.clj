c1d1d164a8511286db1127218f25e07158cd1868
Dave Liepmann <dave.liepmann@gmail.com>
2016-06-29 17:28:28 -0400

    Add project description. Nix unused dependency.

--- project.clj
+++ project.clj
@@ -1,13 +1,8 @@
 (defproject xray-charlie-charlie "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Clojure wrapper of MarkLogic XCC/J client API"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.marklogic/marklogic-xcc "8.0.5"]
-                 ;; marklogic does not require the following, so we
-                 ;; have to pull it ourselves, even if we do not USE
-                 ;; IT. being used in a file that types a static
-                 ;; method parameter with this is enough.
-                 [com.fasterxml.jackson.core/jackson-databind "2.6.3"]]
+                 [com.marklogic/marklogic-xcc "8.0.5"]]
   :repositories [["MarkLogic-releases" "http://developer.marklogic.com/maven2"]])
