e5d22f346bba4e094d710259120c7329aa721c78
Dave Liepmann <dave.liepmann@gmail.com>
2016-07-15 11:13:33 -0400

    Add secretly-necessary jackson-databind dependency

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.marklogic/marklogic-xcc "8.0.5"]
+                 ;; required but not included by MarkLogic (e.g. for ContentFactory):
+                 [com.fasterxml.jackson.core/jackson-databind "2.6.3"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.xml "0.0.8"]]
   :repositories [["MarkLogic-releases" "http://developer.marklogic.com/maven2"]])
