18b0cce847c32dd3233bc00c920a20b52dbc0f49
Dave Liepmann <dave.liepmann@gmail.com>
2016-08-04 16:29:00 +0200

    Version 0.2.7
    A variety of minor improvements: a rudimentary `spawn-module` fn; maps of various classes and enumerations; lots of additional tests. Many refactorings, including streamlining away most repetitive when-lets. Now supports session creation using a variety of different methods, rather than just URI.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uruk "0.2.6"
+(defproject uruk "0.2.7"
   :description "Clojure wrapper of MarkLogic XML Content Connector For Java (XCC/J)"
   :url "https://github.com/daveliepmann/uruk"
   :license {:name "Eclipse Public License"
