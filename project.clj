585f728324f2e16f49f27f5bf5af5064b2446ff4
Dave Liepmann <dave.liepmann@gmail.com>
2017-02-09 13:23:15 +0100

    Bump to version 0.3.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uruk "0.3.3"
+(defproject uruk "0.3.4"
   :description "Clojure wrapper of MarkLogic XML Content Connector For Java (XCC/J)"
   :url "https://github.com/daveliepmann/uruk"
   :license {:name "Eclipse Public License"
@@ -10,6 +10,6 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.xml "0.1.0-beta2"]
                  [slingshot "0.12.2"]]
-  :checksum :warn ;; TODO remove this workaround -- MarkLogic's Maven
-                  ;; repo doesn't have checksums?
+  :checksum :warn
+  ;; TODO remove this workaround -- MarkLogic's Maven repo doesn't have checksums?
   :repositories [["MarkLogic-releases" "http://developer.marklogic.com/maven2"]])
