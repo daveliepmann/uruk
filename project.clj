bcad982d99f7b71c454190d8db3f5d609e3c097d
Dave Liepmann <dave.liepmann@gmail.com>
2017-03-12 22:08:15 +0100

    Use local repository for ML9

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.marklogic/marklogic-xcc "8.0.6"]
+                 [com.marklogic/marklogic-xcc "9.0.20170113"]
+                 ;; [com.marklogic/marklogic-xcc "8.0.6"]
                  ;; required but not included by MarkLogic (e.g. for ContentFactory):
                  [com.fasterxml.jackson.core/jackson-databind "2.6.3"]
                  [org.clojure/data.json "0.2.6"]
@@ -12,4 +13,6 @@
                  [slingshot "0.12.2"]]
   :checksum :warn
   ;; TODO remove this workaround -- MarkLogic's Maven repo doesn't have checksums?
-  :repositories [["MarkLogic-releases" "http://developer.marklogic.com/maven2"]])
+  :repositories [["MarkLogic-releases" "http://developer.marklogic.com/maven2"]
+                 ["local" "file:jar"]]
+  :plugins [[lein-localrepo "0.5.3"]])
