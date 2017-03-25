82ffbc1fb2d1fe0b6045c57e79b71eb3772c54a3
Dave Liepmann <dave.liepmann@gmail.com>
2017-03-25 22:57:49 +0100

    Bump version to 0.3.6
    This version introduces `insert-string`, similar to `insert-element`, for direct creation of documents based on literal Strings.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uruk "0.3.5"
+(defproject uruk "0.3.6"
   :description "Clojure wrapper of MarkLogic XML Content Connector For Java (XCC/J)"
   :url "https://github.com/daveliepmann/uruk"
   :license {:name "Eclipse Public License"
