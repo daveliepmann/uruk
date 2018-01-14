f32a18955febdce035195d17064d7130d6875a1b
Dave Liepmann <dave.liepmann@gmail.com>
2018-01-14 12:17:14 +0100

    Bump version to 0.3.10 and update dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject uruk "0.3.9"
+(defproject uruk "0.3.10"
   :description "Clojure wrapper of MarkLogic XML Content Connector For Java (XCC/J)"
   :url "https://github.com/daveliepmann/uruk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.marklogic/marklogic-xcc "9.0.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.marklogic/marklogic-xcc "9.0.3"]
                  ;; required but not included by MarkLogic (e.g. for ContentFactory):
-                 [com.fasterxml.jackson.core/jackson-databind "2.6.3"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.9.3"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.xml "0.1.0-beta2"]
                  [slingshot "0.12.2"]]
