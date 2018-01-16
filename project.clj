fd720684ad8c061737a5ed87925b2d962f9008dd
Dave Liepmann <dave.liepmann@gmail.com>
2018-01-16 11:20:38 +0100

    Bump to 0.3.11-SNAPSHOT
    Easier to work with fixes as they come in--see https://github.com/daveliepmann/uruk/pull/4#issue-165960291

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uruk "0.3.10"
+(defproject uruk "0.3.11-SNAPSHOT"
   :description "Clojure wrapper of MarkLogic XML Content Connector For Java (XCC/J)"
   :url "https://github.com/daveliepmann/uruk"
   :license {:name "Eclipse Public License"
